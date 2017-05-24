498bbf23732726a5462ec7302390df498585de34
DESKTOP-22A0EPS\user <mdyu1000@gmail.com>
2017-05-25 01:23:34 +0800

    完成倒數計時 剩下排版

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 var isClick = false;
+var ok = false;
+var c = 60;
+var t;
+var unit = $(".unit");
 
 $('.exchange').click(function() {
   if (!isClick) {
     $('.confirm').addClass('confirm_display');
     isClick = true;
-  } /*else {
-    $('.share_icons').removeClass('share_icons_display');
-    isClick = false;
-  }*/
+    ok = false;
+  }
 });
+
+$('.confirm_OK').click(function() {
+  if (!ok) {
+    $('.confirm').removeClass('confirm_display');
+    ok = true;
+    isClick = false;
+    timedCount();
+  }
+});
+
+
+
+function timedCount() {
+  c = c - 1;
+  if (c == 0) {
+    c = 0;
+  }
+  t = setTimeout("timedCount()", 1000);
+  unit.text(c);
+}
