8261bbb42667de5629378ead53e90ae422dad795
DESKTOP-22A0EPS\user <mdyu1000@gmail.com>
2017-05-25 02:22:03 +0800

    完工

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $('.exchange').click(function() {
     $('.confirm').addClass('confirm_display');
     isClick = true;
     ok = false;
+    $('.big_img').addClass('big_img_dark');
   }
 });
 
@@ -18,10 +19,10 @@ $('.confirm_OK').click(function() {
     ok = true;
     isClick = false;
     timedCount();
+    $('.count').addClass('count_display');
+    $('.big_img').removeClass('big_img_dark');
   }
 });
-
-
 
 function timedCount() {
   c = c - 1;
